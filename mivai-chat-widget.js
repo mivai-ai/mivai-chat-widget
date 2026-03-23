ea4fbaf53919f98be6268072e44275c68d939a87
mivai-ai <mivaieu@gmail.com>
2026-03-23 19:02:52 +0100

    Update mivai-chat-widget.js

--- mivai-chat-widget.js
+++ mivai-chat-widget.js
@@ -2,7 +2,7 @@
 window.addEventListener("load", function () {
   (function () {
     // ==========================
-    // CONFIG DALLO <script> (data-*)
+    // CONFIG DALLO <script> v2 (data-*)
     // ==========================
 
     function findMivaiScript() {
